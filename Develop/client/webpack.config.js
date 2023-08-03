bd6fe4493a115551aa8b46fa8f87d9a772e878b0
Lizzz <alejandroraidi@gmail.com>
2023-08-03 14:22:25 -0600

    client webpack config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,12 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
+      //Service Worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: '.src-sw.js',
+      }),
+      //Manifest File
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor on the web',
+        background_color: '#31a9e1',
+        theme_color: '#31a9e1',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
+      //CSS loader
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use:{
+            loader: 'babel-loader',
+            options:{
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread','@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
